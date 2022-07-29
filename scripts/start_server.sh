dd1e186349a79c1b2a0f75877ad261ba86c545bc
priya-optimists <86224496+priya-optimists@users.noreply.github.com>
2022-07-30 01:58:43 +0530

    Update start scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export HOME="/home/ubuntu/"
 export PM2_HOME=/home/ubuntu/.pm2
 pm2 delete testexp
 cd /home/ubuntu/testexp/
