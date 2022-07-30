7c600a42d592c664ef54d60cac411143a1b6e414
Priya <priya@optimists.in>
2022-07-30 10:04:04 +0530

    Modified start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export HOME="/home/ubuntu/"
 export PM2_HOME=/home/ubuntu/.pm2
-pm2 delete testexp
+/home/ubuntu/.nvm/versions/node/v16.16.0/bin/pm2 delete testexp
 cd /home/ubuntu/testexp/
-pm2 start --name testexp npm -- start
+/home/ubuntu/.nvm/versions/node/v16.16.0/bin/pm2 start --name testexp npm -- start
