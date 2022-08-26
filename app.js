29a8a3b9c9afed0fa2b2a8038971af755df7d1f6
PriyaVarshini K <86224496+priya-optimists@users.noreply.github.com>
2022-08-26 18:45:58 +0530

    v1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Application is running on ec2 instance now')
+  res.send('Application is running on ec2 instance now - version 1')
 })
 
 app.listen(port, () => {
